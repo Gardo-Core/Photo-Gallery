037c41449c4a667384f838c90e5e023b93f3a106
Gardo-Core <reclamationfire@gmail.com>
2025-12-08 13:53:35 +0100

    Spostato titolo in alto come intestazione e aggiunte nuove immagini

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,17 @@ export default function Home() {
 		{ src: '/6.webp', alt: 'Image 6' },
 		{ src: '/7.webp', alt: 'Image 7' },
 		{ src: '/8.webp', alt: 'Image 8' },
+		{ src: '/9.webp', alt: 'Image 9' },
+		{ src: '/10.webp', alt: 'Image 10' },
+		{ src: '/11.webp', alt: 'Image 11' },
+		{ src: '/12.webp', alt: 'Image 12' },
+		{ src: '/13.webp', alt: 'Image 13' },
+		{ src: '/14.webp', alt: 'Image 14' },
+		{ src: '/15.webp', alt: 'Image 15' },
+		{ src: '/16.webp', alt: 'Image 16' },
+		{ src: '/17.webp', alt: 'Image 17' },
+		{ src: '/18.webp', alt: 'Image 18' },
+		{ src: '/19.webp', alt: 'Image 19' },
 	];
 
 	return (
@@ -22,7 +33,7 @@ export default function Home() {
 				falloff={{ near: 0.8, far: 14 }}
 				className="h-screen w-full rounded-lg overflow-hidden"
 			/>
-			<div className="h-screen inset-0 pointer-events-none fixed flex items-center justify-center text-center px-3 mix-blend-exclusion text-white">
+			<div className="h-screen inset-0 pointer-events-none fixed flex items-start justify-center text-center px-3 mix-blend-exclusion text-white pt-10">
 				<h1 className="font-serif text-4xl md:text-7xl tracking-tight">
 					<span className="italic">Gardo; </span> Photo Gallery
 				</h1>
